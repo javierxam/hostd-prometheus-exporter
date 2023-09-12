725a3dcf816f349cb1e6871e3255cb267dd894f8
David <>
2023-09-12 21:36:12 +0200

    New metrics for revenue and contracts

--- collector.go
+++ collector.go
@@ -3,13 +3,15 @@ package main
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+
 	//	"fmt"
-	rhp2 "go.sia.tech/core/rhp/v2"
-	"go.sia.tech/core/types"
-	"go.sia.tech/hostd/api"
 	"log"
 	"math/big"
 	"time"
+
+	rhp2 "go.sia.tech/core/rhp/v2"
+	"go.sia.tech/core/types"
+	"go.sia.tech/hostd/api"
 )
 
 var (
@@ -56,6 +58,47 @@ var (
 		Name: "hostd_failed_contract_count", Help: "Number of failed contracts"})
 	hostdSuccessfulContractCount = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "hostd_successful_contract_count", Help: "Number of successful contracts"})
+
+	hostdContractPrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_contract_price", Help: "Contract price"})
+	hostdIngressPrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_ingress_price", Help: "Ingress price"})
+	hostdEgressPrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_egress_price", Help: "Egress price"})
+	hostdBaseRPCPrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_baserpc_price", Help: "BaseRPC price"})
+	hostdSectorAccessPrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_sector_access_price", Help: "SectorAccess price"})
+	hostdStoragePrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_storage_price", Help: "Storage price"})
+	hostdCollateralMultiplier = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_collateral_multiplier", Help: "Collateral Multiplier"})
+
+	hostdRevenueEarnedRPC = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_earned_rpc", Help: "Revenue earned for RPC"})
+	hostdRevenueEarnedStorage = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_earned_storage", Help: "Revenue earned for storage"})
+	hostdRevenueEarnedIngress = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_earned_ingress", Help: "Revenue earned for ingress"})
+	hostdRevenueEarnedEgress = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_earned_egress", Help: "Revenue earned for egress"})
+	hostdRevenueEarnedRegistryRead = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_earned_registry_read", Help: "Revenue earned for registry reads"})
+	hostdRevenueEarnedRegistryWrite = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_earned_registry_write", Help: "Revenue earned for registry writes"})
+
+	hostdRevenuePotentialRPC = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_potential_rpc", Help: "Potential revenue for RPC"})
+	hostdRevenuePotentialStorage = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_potential_storage", Help: "Potential revenue for storage"})
+	hostdRevenuePotentialIngress = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_potential_ingress", Help: "Potential revenue for ingress"})
+	hostdRevenuePotentialEgress = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_potential_egress", Help: "Potential revenue for egress"})
+	hostdRevenuePotentialRegistryRead = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_potential_registry_read", Help: "Potential revenue for registry reads"})
+	hostdRevenuePotentialRegistryWrite = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "hostd_revenue_potential_registry_write", Help: "Potential revenue for registry writes"})
 )
 
 func convertCurrency(c types.Currency) float64 {
@@ -70,27 +113,53 @@ func callClient(passwd string, address string) {
 		log.Fatalln(err)
 	}
 
+	// Storage
 	hostdTotalStorage.Set(float64((metrics.Storage.TotalSectors) * rhp2.SectorSize))
 	hostdUsedStorage.Set(float64((metrics.Storage.PhysicalSectors) * rhp2.SectorSize))
 	contractStorage.Set(float64((metrics.Storage.ContractSectors) * rhp2.SectorSize))
 	tempStorage.Set(float64((metrics.Storage.TempSectors) * rhp2.SectorSize))
-
 	storageReads.Set(float64(metrics.Storage.Reads))
 	storageWrites.Set(float64(metrics.Storage.Writes))
-
 	hostdRemainingStorage.Set(float64((metrics.Storage.TotalSectors - metrics.Storage.PhysicalSectors) * rhp2.SectorSize))
 
+	// Data
 	hostdIngress.Set(float64(metrics.Data.RHP2.Ingress + metrics.Data.RHP3.Ingress))
 	hostdEgress.Set(float64(metrics.Data.RHP2.Egress + metrics.Data.RHP3.Egress))
 
-	hostdLockedCollateral.Set(convertCurrency(metrics.Contracts.LockedCollateral))
-	hostdRiskedCollateral.Set(convertCurrency(metrics.Contracts.RiskedCollateral))
-
+	// Balance
 	walletConfirmedSiacoinBalance.Set(convertCurrency(metrics.Balance))
 
+	// Contracts
+	hostdLockedCollateral.Set(convertCurrency(metrics.Contracts.LockedCollateral))
+	hostdRiskedCollateral.Set(convertCurrency(metrics.Contracts.RiskedCollateral))
 	hostdPendingContractCount.Set(float64(metrics.Contracts.Pending))
 	hostdActiveContractCount.Set(float64(metrics.Contracts.Active))
 	hostdRejectedContractCount.Set(float64(metrics.Contracts.Rejected))
 	hostdFailedContractCount.Set(float64(metrics.Contracts.Failed))
 	hostdSuccessfulContractCount.Set(float64(metrics.Contracts.Successful))
+
+	// Pricing
+	hostdContractPrice.Set(convertCurrency(metrics.Pricing.ContractPrice))
+	hostdIngressPrice.Set(convertCurrency(metrics.Pricing.IngressPrice))
+	hostdEgressPrice.Set(convertCurrency(metrics.Pricing.EgressPrice))
+	hostdBaseRPCPrice.Set(convertCurrency(metrics.Pricing.BaseRPCPrice))
+	hostdSectorAccessPrice.Set(convertCurrency(metrics.Pricing.SectorAccessPrice))
+	hostdStoragePrice.Set(convertCurrency(metrics.Pricing.StoragePrice))
+	hostdCollateralMultiplier.Set(float64(metrics.Pricing.CollateralMultiplier))
+
+	// Revenue Earned
+	hostdRevenueEarnedRPC.Set(convertCurrency(metrics.Revenue.Earned.RPC))
+	hostdRevenueEarnedStorage.Set(convertCurrency(metrics.Revenue.Earned.Storage))
+	hostdRevenueEarnedIngress.Set(convertCurrency(metrics.Revenue.Earned.Ingress))
+	hostdRevenueEarnedEgress.Set(convertCurrency(metrics.Revenue.Earned.Egress))
+	hostdRevenueEarnedRegistryRead.Set(convertCurrency(metrics.Revenue.Earned.RegistryRead))
+	hostdRevenueEarnedRegistryWrite.Set(convertCurrency(metrics.Revenue.Earned.RegistryWrite))
+
+	// Revenue Potential
+	hostdRevenuePotentialRPC.Set(convertCurrency(metrics.Revenue.Potential.RPC))
+	hostdRevenuePotentialStorage.Set(convertCurrency(metrics.Revenue.Potential.Storage))
+	hostdRevenuePotentialIngress.Set(convertCurrency(metrics.Revenue.Potential.Ingress))
+	hostdRevenuePotentialEgress.Set(convertCurrency(metrics.Revenue.Potential.Egress))
+	hostdRevenuePotentialRegistryRead.Set(convertCurrency(metrics.Revenue.Potential.RegistryRead))
+	hostdRevenuePotentialRegistryWrite.Set(convertCurrency(metrics.Revenue.Potential.RegistryWrite))
 }
